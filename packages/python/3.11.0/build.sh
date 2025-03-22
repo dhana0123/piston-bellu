c15a80a14e6835734de90925b53a509de283b7b0
b santhosh <b.santhosh.dhana@gmail.com>
2025-03-22 03:53:06 -0700

    removed tensorflow

--- packages/python/3.11.0/build.sh
+++ packages/python/3.11.0/build.sh
@@ -18,4 +18,4 @@ cd ..
 
 rm -rf build
 
-bin/pip3 install numpy scipy pandas pycryptodome whoosh bcrypt passlib sympy xxhash base58 cryptography PyNaCl tensorflow matplotlib
+bin/pip3 install numpy scipy pandas pycryptodome whoosh bcrypt passlib sympy xxhash base58 cryptography PyNaCl matplotlib
